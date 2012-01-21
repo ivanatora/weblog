fd9cc9439409940ff5b29ae3e0e6999f315ef10e
Ivan Petrushev <ivanatora@gmail.com>
2012-01-21 18:39:51 +0200

    sidebar splicing

---
+++ application/views/include/sidebar_categories.php
@@ -0,0 +1,13 @@
+<h3>Categories</h3>
+
+        <ul class="categories">
+            <li><span><a href="#">Programs</a></span></li>
+            <li><span><a href="#">Student Info</a></span></li>
+            <li><span><a href="#">Teachers</a></span></li>
+            <li><span><a href="#">Descriptions</a></span></li>
+            <li><span><a href="#">Administrators</a></span></li>
+
+            <li><span><a href="#">Basic Information</a></span></li>
+            <li><span><a href="#">Vacancies</a></span></li>
+            <li class="last"><span><a href="#">Calendar</a></span></li>
+         </ul>
---
+++ application/views/include/sidebar_newsletter.php
@@ -0,0 +1,10 @@
+         <form action="" id="newsletter-form">
+            <fieldset>
+            <div class="rowElem">
+
+               <h2>Newsletter</h2>
+               <input type="email" value="Enter Your Email Here" onFocus="if(this.value=='Enter Your Email Here'){this.value=''}" onBlur="if(this.value==''){this.value='Enter Your Email Here'}" >
+               <div class="clear"><a href="#" class="fleft">Unsubscribe</a><a href="#" class="fright" onClick="document.getElementById('newsletter-form').submit()">Submit</a></div>
+            </div>
+            </fieldset>
+         </form>
